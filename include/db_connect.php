b61d26be4e0f29455c7b7a55c392092320a83231
ratik chandna <ratikchandna@yahoo.com>
2014-07-07 19:08:52 +0530

    Update db_connect.php

--- include/db_connect.php
+++ include/db_connect.php
@@ -1,4 +1,4 @@
 <?php
 include_once 'psl-config.php';   // As functions.php is not included
-$mysqli = new mysqli("mysql16.000webhost.com", "a8743500_secure", "ipad12345", "a8743500_secure");
+$mysqli = new mysqli("hostname", "username", "password", "database");
 ?>
